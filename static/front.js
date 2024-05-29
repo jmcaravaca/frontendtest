f52869e11a994b580a8439f6c0eba94169f0b369
Jorge <jmcaravaca@minsait.com>
2024-05-29 16:41:28 +0200

    alert updates

--- static/front.js
+++ static/front.js
@@ -30,23 +30,32 @@
     }
 }
 
+function showAlert(alertType, alertText) {
+    //'alert-warning', 'alert-success'
+    const alertDiv = document.createElement('div');
+    alertDiv.classList.add('alert', 'alert-dismissible', 'fade', 'show');
+    alertDiv.classList.add(alertType);
+    alertDiv.innerHTML = `
+    ${alertText}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+`;
+    const alertArea = document.getElementById('alertArea');
+    alertArea.appendChild(alertDiv);
+    setTimeout(() => {
+        alertDiv.remove();
+    }, 3000);     
+}
+
 function confirmUpload() {
     // Validate file selection (you can add additional validation here)
     const fileInput = document.getElementById('fileInput');
+    // TODO: Add input file extension validation
     if (fileInput.files.length === 0) {
-        const alertDiv = document.createElement('div');
-        alertDiv.classList.add('alert', 'alert-warning', 'alert-dismissible', 'fade', 'show');
-        alertDiv.innerHTML = `
-        Please select a file.
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    `;
-        const alertArea = document.getElementById('alertArea');
-        alertArea.appendChild(alertDiv);
-        setTimeout(() => {
-            alertDiv.remove();
-        }, 3000);        
+        showAlert('alert-warning', "Error: Please select a valid file");
         return;
     }
+    // If file is valid
+    showAlert('alert-success', "Success: Processing file");
     // Send an HTTP request to the specified endpoint (adjust the URL as needed)
     const formData = new FormData(document.getElementById('uploadForm'));
     fetch('/infer/upload', {
